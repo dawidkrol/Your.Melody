fa7d44c2ccccd27c8448c8782da7f8ca68f9e2c0
dawidkrol7 <dawidkrol7@gmail.com>
2022-11-13 19:45:00 +0100

    Creating library project

---
+++ Your.Melody.Library/DbAccess/ISqlDataAccess.cs
@@ -0,0 +1,10 @@
+﻿namespace Your.Melody.Library.DbAccess
+{
+    public interface ISqlDataAccess
+    {
+        Task<IEnumerable<T>> LoadDataAsync<T, U>(string storedProcedure, U parameters, string connectionId = "");
+        Task<IEnumerable<T>> LoadDataFromViewAsync<T>(string storedProcedure, string connectionId = "");
+        Task<IEnumerable<T>> LoadMultipleMapDataAsync<T, U, O>(string storedProcedure, U parameters, Func<T, O, T> func, string connectionId = "");
+        Task SaveDataAsync<T>(string storedProcedire, T parameters, string connectionId = "");
+    }
+}
---
+++ Your.Melody.Library/DbAccess/SqlDataAccess.cs
@@ -0,0 +1,45 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Your.Melody.Library.DbAccess
+{
+    public class SqlDataAccess : ISqlDataAccess
+    {
+        private readonly IConfiguration _config;
+
+        public SqlDataAccess(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task<IEnumerable<T>> LoadDataAsync<T, U>(string storedProcedure, U parameters, string connectionId = "")
+        {
+            using IDbConnection connection = new SqlConnection(_config.GetConnectionString(connectionId));
+
+            return await connection.QueryAsync<T>(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+        }
+
+        public async Task<IEnumerable<T>> LoadDataFromViewAsync<T>(string view, string connectionId = "")
+        {
+            using IDbConnection connection = new SqlConnection(_config.GetConnectionString(connectionId));
+
+            return await connection.QueryAsync<T>($"select * from {view}", new { });
+        }
+
+        public async Task<IEnumerable<T>> LoadMultipleMapDataAsync<T, U, O>(string storedProcedure, U parameters, Func<T, O, T> func, string connectionId = "")
+        {
+            using IDbConnection connection = new SqlConnection(_config.GetConnectionString(connectionId));
+
+            return await connection.QueryAsync<T, O, T>(storedProcedure, func, param: parameters, commandType: CommandType.StoredProcedure);
+        }
+
+        public async Task SaveDataAsync<T>(string storedProcedire, T parameters, string connectionId = "")
+        {
+            using IDbConnection connection = new SqlConnection(_config.GetConnectionString(connectionId));
+
+            await connection.ExecuteAsync(storedProcedire, parameters, commandType: CommandType.StoredProcedure);
+        }
+    }
+}
